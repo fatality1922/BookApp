4a5d94bd33fafd6be86f2802bb857cafc209934e
Jan Kowalski <fatality1922@gmail.com>
2020-12-15 15:43:13 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -2,24 +2,27 @@
     'use strict';
 
     const select = {
-        wrapper: '.books-list',
+        wrapper: {
+            booksList: '.books-list',
+        },
         templateOf: {
             booksList: '#template-book',
         },
-    },
+    };
 
     const templates = {
         booksList: Handlebars.compile(document.querySelector(select.templateOf.booksList).innerHTML),
-      };
+    };
 
+    const bookListWrapper = document.querySelector(select.wrapper.booksList);
     const render = function () {
         for (let book of dataSource.books) {
             const generatedHTML = templates.booksList(book);
             console.log(generatedHTML);
             const element = utils.createDOMFromHTML(generatedHTML);
-            booksListWrapper.appendChild(element);
+            bookListWrapper.appendChild(element);
         }
     };
-    
+
     render();
 }
