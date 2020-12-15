000551fd43b8acddece47c44cb82de1cb0d4a102
Jan Kowalski <fatality1922@gmail.com>
2020-12-15 19:18:02 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -8,21 +8,44 @@
         templateOf: {
             booksList: '#template-book',
         },
+        class: {
+            favouriteBook: 'favorite',
+        }
     };
-
+    const favoriteBooks = [];
     const templates = {
         booksList: Handlebars.compile(document.querySelector(select.templateOf.booksList).innerHTML),
     };
 
+
     const bookListWrapper = document.querySelector(select.wrapper.booksList);
+
     const render = function () {
         for (let book of dataSource.books) {
             const generatedHTML = templates.booksList(book);
-            console.log(generatedHTML);
             const element = utils.createDOMFromHTML(generatedHTML);
             bookListWrapper.appendChild(element);
         }
     };
 
+    const initActions = function () {
+        bookListWrapper.addEventListener('dblclick', function (event) {
+            event.preventDefault();
+            const clickedElement = event.target.offsetParent;
+
+            if (clickedElement.classList.contains('book__image')) {
+                const id = clickedElement.getAttribute('data-id');
+                if (!clickedElement.classList.contains(select.class.favouriteBook)) {
+                    favoriteBooks.push(id);
+                    clickedElement.classList.add(select.class.favouriteBook);
+                } else {
+                    favoriteBooks.splice(favoriteBooks.indexOf(id), 1);
+                    clickedElement.classList.remove(select.class.favouriteBook);
+                }
+            }
+        });
+    }
+
     render();
+    initActions();
 }
